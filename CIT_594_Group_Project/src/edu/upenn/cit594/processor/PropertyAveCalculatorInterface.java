1d64c9c133bec58c143b8ed62a8f5230913b57f3
sue0407 <71115736+sue0407@users.noreply.github.com>
2022-12-05 18:44:50 +0900

    Delete PropertyAveCalculatorInterface.java

--- CIT_594_Group_Project/src/edu/upenn/cit594/processor/PropertyAveCalculatorInterface.java
+++
@@ -1,9 +0,0 @@
-package edu.upenn.cit594.processor;
-
-import edu.upenn.cit594.util.PropertyData;
-
-public interface AveCalculator {
-	
-	public int calcAverage(int zipCode, PropertyData propertyData);
-
-}
